12fded70fb8ab58c7eb764989217cf066c7baec6
Aaron Low <aaronlws95@gmail.com>
2020-06-02 00:12:32 +0800

    initial map display with json and simple camera

--- twog/twog/Game1.cs
+++ twog/twog/Game1.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,7 +14,6 @@ namespace twog
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D player;
 
         int virtualWidth = 320;
         int virtualHeight = 180;
@@ -16,6 +21,13 @@ namespace twog
         int actualHeight = 360;
 
         RenderTarget2D renderTarget;
+
+        Texture2D player;
+        List<Texture2D> tileList = new List<Texture2D>();
+
+        List<int> mapTiles;
+
+        Camera2D camera;
 
         public Game1()
         {
@@ -28,6 +40,8 @@ namespace twog
             base.Initialize();
             renderTarget = new RenderTarget2D(GraphicsDevice, virtualWidth, virtualHeight);
 
+            camera = new Camera2D(GraphicsDevice.Viewport);
+
             graphics.PreferredBackBufferWidth = actualWidth;  // set this value to the desired width of your window
             graphics.PreferredBackBufferHeight = actualHeight;   // set this value to the desired height of your window
             graphics.ApplyChanges();
@@ -37,9 +51,18 @@ namespace twog
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            player = Content.Load<Texture2D>("Assets/player-idle");
-        }
 
+            player = Content.Load<Texture2D>("Assets/player-idle");
+
+            string[] tileNames  = { "tile-1", "tile-2", "tile-3", "tile-4"};
+            foreach (var s in tileNames)
+            {
+                tileList.Add(Content.Load<Texture2D>("Assets/Tiles/" + s));
+            }
+
+            Map map = new Map("Maps/test_map.json");
+            mapTiles = map.mapjson.Tiles;
+        }
 
         protected override void UnloadContent()
         {
@@ -47,8 +70,23 @@ namespace twog
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var keyboardState = Keyboard.GetState();
+
+            var camSpeed = 2;
+
+            // movement
+            if (keyboardState.IsKeyDown(Keys.Up))
+                camera.Position -= new Vector2(0, camSpeed);
+
+            if (keyboardState.IsKeyDown(Keys.Down))
+                camera.Position += new Vector2(0, camSpeed);
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+                camera.Position -= new Vector2(camSpeed, 0);
+
+            if (keyboardState.IsKeyDown(Keys.Right))
+                camera.Position += new Vector2(camSpeed, 0);
 
             base.Update(gameTime);
         }
@@ -57,18 +95,26 @@ namespace twog
         {
             GraphicsDevice.SetRenderTarget(renderTarget);
             GraphicsDevice.Clear(Color.Black);
-            spriteBatch.Begin();
-            spriteBatch.Draw(player, new Vector2(0, 0), Color.White);
-            spriteBatch.Draw(player, new Vector2(virtualWidth/2, virtualHeight/2), Color.White);
+            Matrix viewMatrix = camera.GetViewMatrix();
+            spriteBatch.Begin(transformMatrix: viewMatrix);
+
+            for (int i = 0; i < 50; ++i)
+            {
+                for (int j = 0; j < 50; ++j)
+                {
+                    int lin_idx = i * 50 + j;
+                    spriteBatch.Draw(tileList[mapTiles[lin_idx]], new Vector2(16 * i, 16 * j), Color.White);
+                }
+            }
+            
+            spriteBatch.Draw(player, new Vector2(16 * 10, 16 * 6), Color.White);
             spriteBatch.End();
 
             GraphicsDevice.SetRenderTarget(null);
-           
+
             spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null);
             spriteBatch.Draw(renderTarget, new Rectangle(0, 0, actualWidth, actualHeight), Color.White);
             spriteBatch.End();
-
-            
 
             base.Draw(gameTime);
         }
---
+++ twog/twog/Map.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace twog
+{
+    public class MapJson
+    {
+        public string Name;
+        public List<int> Tiles; 
+    }
+
+    public class Map
+    {
+        public MapJson mapjson;
+
+        public Map(string map_file)
+        {
+            LoadJson(map_file);
+        }
+
+        private void LoadJson(string map_file)
+        {
+            using (StreamReader r = new StreamReader(map_file))
+            {
+                string json = r.ReadToEnd();
+                mapjson = JsonConvert.DeserializeObject<MapJson>(json);
+            }
+        }
+    }
+}
